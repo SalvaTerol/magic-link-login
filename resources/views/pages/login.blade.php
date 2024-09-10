1d2980b72a9228a1aaeb9ebf46744f8f3d376d5a
salva <salvaterolsl@gmail.com>
2024-09-10 18:50:44 +0200

    WIP

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">
             <div>
                 <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -27,7 +27,7 @@
                 </div>
             @endif
 
-            <div class="space-y-6">
+            <div class="mt-8 space-y-6">
                 <livewire:magic-link-login::login/>
             </div>
         </div>
