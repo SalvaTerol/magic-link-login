77a664e9bdca89d3905dbd31dd93accdec3a10b0
salva <salvaterolsl@gmail.com>
2024-09-10 18:36:30 +0200

    WIP

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use SalvaTerol\MagicLinkLogin\Controllers\AuthController;
 
 Route::middleware(['web', 'guest'])->group(callback: function () {
     Route::view('/login', 'magic-link-login::pages.login')->name('login');
-    Route::get('/auth/{service}/callback', [AuthController::class, 'handleProviderCallback'])->name('login.callback');
+    Route::get('/auth/callback/{service}', [AuthController::class, 'handleProviderCallback'])->name('login.callback');
     Route::get('/auth/{service}', [AuthController::class, 'redirectToProvider'])->name('login.redirect');
     Route::get('/login/{token}', [AuthController::class, 'loginWithToken'])->name('login.token')->middleware('signed');
 });
